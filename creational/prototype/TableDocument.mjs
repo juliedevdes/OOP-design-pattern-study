8f6c2bb7feb045df64418d6f52f8191598c4fc3e
yuliia_okhrimenko <yuliia.okhrimenko@valtech.com>
2024-10-25 18:45:40 +0300

    comments fix

--- creational/prototype/TableDocument.mjs
+++
@@ -1,8 +0,0 @@
-import { Document } from "./DocumentPrototype.mjs";
-
-export class TableDocument extends Document {
-  constructor() {
-    super();
-    this.type = "Tables";
-  }
-}
--- creational/prototype/TextDocument.mjs
+++
@@ -1,8 +0,0 @@
-import { Document } from "./DocumentPrototype.mjs";
-
-export class TextDocument extends Document {
-  constructor() {
-    super();
-    this.type = "Text";
-  }
-}
